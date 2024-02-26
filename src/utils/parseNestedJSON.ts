cd7034afdbca1916c5054a004d135d199971abd4
Artem Skorbezh <artyom.ireday@gmail.com>
2024-02-27 00:01:09 +0300

    refactor: parse json

--- src/utils/parseNestedJSON.ts
+++ src/utils/parseNestedJSON.ts
@@ -1,20 +1,31 @@
-interface PObj {
-  [key: string]: PObj | string;
-}
-
-const parseNestedJSON = (jsonStr: string): PObj => {
-  const parsedData = JSON.parse(jsonStr);
-
-  const parseNested = (obj: PObj): PObj => {
-    Object.entries(obj).forEach(([prop, value]) => {
-      if (typeof value === 'string' && value.includes('{')) {
-        obj[prop] = parseNested(JSON.parse(value as string));
-      }
-    });
-    return obj;
+const parseNestedJSON = (str: string) => {
+  const parsePropertyValue = (value: string | unknown): unknown => {
+    if (typeof value === 'string' && value.includes('{')) {
+      return parseNestedJSON(value);
+    }
+    return value;
   };
 
-  return parseNested(parsedData);
+  const parseObjectProperties = (
+    obj: Record<string, string | unknown>,
+  ): Record<string, unknown> => {
+    const parsedObject: Record<string, unknown> = {};
+    for (const property in obj) {
+      parsedObject[property] = parsePropertyValue(obj[property]);
+    }
+    return parsedObject;
+  };
+
+  try {
+    const data = JSON.parse(str);
+    if (typeof data === 'object' && data !== null) {
+      return parseObjectProperties(data);
+    }
+    return data;
+  } catch (error) {
+    console.error('Error parsing JSON:', error);
+    return str;
+  }
 };
 
 export default parseNestedJSON;
